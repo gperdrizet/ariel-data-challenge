6e7cac472b6e8b51f635c8e2ea5e5b636d68386c
George Perdrizet <george@perdrizet.org>
2025-09-02 10:18:03 -0400

    Cleaned up configuration file

--- configuration.py
+++ configuration.py
@@ -10,13 +10,8 @@ import os
 COMPETITION_NAME = 'ariel-data-challenge-2025'
 
 # Data paths
-DATA_DIRECTORY = './data/fast_scratch'
-RAW_DATA_DIRECTORY = f'{DATA_DIRECTORY}/raw_data'
-SUBMISSION_DIRECTORY = f'{DATA_DIRECTORY}/submission'
-
-# Data files
-TRAINING_DATA_FILE = f'{RAW_DATA_DIRECTORY}/train.csv'
-SUBMISSION_DATA_FILE = f'{RAW_DATA_DIRECTORY}/test.csv'
+DATA_DIRECTORY = './data'
+RAW_DATA_DIRECTORY = f'{DATA_DIRECTORY}/raw'
 
 #############################################################
 # Optuna RDB credentials ####################################
