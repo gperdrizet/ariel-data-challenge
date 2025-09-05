b660c01f26705e414baee5c430dd58752ff04195
George Perdrizet <george@perdrizet.org>
2025-09-05 00:12:32 -0400

    Finished adding a test frame to the tests directory

---
+++ tests/test_signal_correction.py
@@ -0,0 +1,10 @@
+'''Unittests for signal correction class'''
+
+import unittest
+
+from data_preprocessing.signal_correction import SignalCorrection
+
+class TestSignalCorrection(unittest.TestCase):
+
+    def setUp(self):
+        pass
