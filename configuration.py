2b7fc430f468c78cbb9f0b996931f72dc168bd6d
George Perdrizet <george@perdrizet.org>
2025-09-08 20:35:38 -0400

    Added figure directory to config file

--- configuration.py
+++ configuration.py
@@ -15,10 +15,10 @@ RAW_DATA_DIRECTORY = f'{DATA_DIRECTORY}/raw'
 METADATA_DIRECTORY = f'{DATA_DIRECTORY}/metadata'
 SIGNAL_CORRECTED_DIRECTORY = f'{DATA_DIRECTORY}/signal_corrected'
 EXPERIMENT_RESULTS_DIRECTORY = f'{DATA_DIRECTORY}/experiment_results'
+FIGURES_DIRECTORY = './figures'
 
 SAMPLE_PLANET = '342072318'
 
-FIGURES_DIRECTORY = './figures'
 
 #############################################################
 # Figure export #############################################
