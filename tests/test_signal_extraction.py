78d0ca5c6ab8d8fc010c9a3fee12ca726546c749
George Perdrizet <george@perdrizet.org>
2025-09-12 23:00:02 -0400

    Started working on signal extraction class

---
+++ tests/test_signal_extraction.py
@@ -0,0 +1,9 @@
+'''Unittests for signal extraction class'''
+
+import unittest
+
+import numpy as np
+import pandas as pd
+import h5py
+
+from ariel_data_preprocessing.signal_extraction import SignalExtraction
