f55b7fcb84263572144d0cdc3ed75564dfddd6b9
George Perdrizet <george@perdrizet.org>
2025-09-18 01:27:03 -0400

    Refactored model training pipeline

--- model_training/functions/utils.py
+++ model_training/functions/utils.py
@@ -1,6 +1,7 @@
 '''Helper functions for model training'''
 
 # Standard library imports
+import datetime
 import random
 from functools import partial
 
@@ -11,6 +12,7 @@ import tensorflow as tf
 
 # Local imports
 import configuration as config
+from model_training.functions.model_definitions import cnn
 
 
 def data_loader(planet_ids: list, data_file: str, sample_size: int = 100):
@@ -88,3 +90,75 @@ def create_datasets(
     )
 
     return training_dataset, validation_dataset
+
+
+def training_run(
+        training_planet_ids: list,
+        validation_planet_ids: list,
+        sample_size: int,
+        learning_rate: float,
+        l1: float,
+        l2: float,
+        first_filter_set: int,
+        second_filter_set: int,
+        third_filter_set: int,
+        filter_size: int,
+        batch_size: int,
+        steps: int
+) -> float:
+    '''Function to run a single training session with fixed hyperparameters.'''
+
+    # Create the training and validation datasets
+    training_dataset, validation_dataset = create_datasets(
+        training_planet_ids,
+        validation_planet_ids,
+        sample_size=sample_size
+    )
+
+    # Build the CNN model with the suggested hyperparameters
+    model = cnn(
+        frames=sample_size,
+        wavelengths=config.WAVELENGTHS,
+        learning_rate=learning_rate,
+        l1=l1,
+        l2=l2,
+        filter_nums=[first_filter_set, second_filter_set, third_filter_set],
+        filter_size=filter_size
+    )
+
+    # Set early stopping callback
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        patience=10,
+        min_delta=0.000005,
+        mode='min',
+        verbose=1,
+        restore_best_weights=True
+    )
+
+    # Set tensorboard callback
+    log_dir = config.TENSORBOARD_LOG_DIR + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir,
+        histogram_freq=1
+    )
+
+    # Train the model
+    model.fit(
+        training_dataset.batch(batch_size),
+        validation_data=validation_dataset.batch(batch_size),
+        epochs=config.EPOCHS,
+        steps_per_epoch=steps,
+        validation_steps=steps,
+        verbose=0,
+        callbacks=[early_stopping_callback, tensorboard_callback]
+    )
+
+    # Evaluate the model on the validation dataset and return the RMSE
+    rmse = model.evaluate(
+        validation_dataset.batch(batch_size),
+        return_dict=True,
+        verbose=0
+    )['RMSE']
+
+    return rmse
--- model_training/optimize_cnn.py
+++ model_training/optimize_cnn.py
@@ -1,23 +1,29 @@
 '''Optimize CNN model hyperparameters using Optuna and visualize results with Optuna Dashboard'''
 
 # Standard library imports
-import datetime
 import random
-from functools import partial
+import shutil
+from pathlib import Path
 
 # Third party imports
 import h5py
 import optuna
-import tensorflow as tf
 
 # Local imports
 import configuration as config
-from model_training.functions.model_definitions import cnn
-from model_training.functions.utils import create_datasets
+from model_training.functions.utils import training_run
 
 
 def run():
     '''Main function to start Optuna optimization run.'''
+
+    # Deal with tensorboard log directory
+    try:
+        shutil.rmtree(config.TENSORBOARD_LOG_DIR)
+    except FileNotFoundError:
+        pass
+    
+    Path(config.TENSORBOARD_LOG_DIR).mkdir(parents=True, exist_ok=True)
 
     # Load corrected/extracted data for a sample planet
     with h5py.File(f'{config.PROCESSED_DATA_DIRECTORY}/train.h5', 'r') as hdf:
@@ -37,93 +43,34 @@ def run():
         study_name='cnn_optimization',
         direction='minimize',
         storage=storage_name,
-        load_if_exists=True
+        load_if_exists=False
     )
 
     study.optimize(
-        lambda trial: objective(trial, training_planet_ids, validation_planet_ids),
+        lambda trial: objective(
+            trial,
+            training_planet_ids,
+            validation_planet_ids
+        ),
         n_trials=10000
     )
 
 def objective(trial, training_planet_ids, validation_planet_ids) -> float:
     '''Objective function for Optuna CNN hyperparameter optimization.'''
 
-    # Optimize the sample size used in the data generator
-    sample_size = trial.suggest_categorical('sample_size', [50, 100, 200, 500])
-
-    # Create the training and validation datasets
-    training_dataset, validation_dataset = create_datasets(
+    rmse = training_run(
         training_planet_ids,
         validation_planet_ids,
-        sample_size=sample_size
+        trial.suggest_categorical('sample_size', [50, 100, 200, 500]),
+        trial.suggest_float('learning_rate', 1e-5, 1e-2, log=True),
+        trial.suggest_float('l_one', 1e-10, 1e-2, log=True),
+        trial.suggest_float('l_two', 1e-10, 1e-2, log=True),
+        trial.suggest_categorical('first_filter_set', (8, 16, 32, 64)),
+        trial.suggest_categorical('second_filter_set', (16, 32, 64, 128)),
+        trial.suggest_categorical('third_filter_set', (32, 64, 128, 256)),
+        trial.suggest_categorical('filter_size', (2, 3, 4, 5)),
+        trial.suggest_categorical('batch_size', (1, 2, 4, 8, 16, 32, 64, 128)),
+        trial.suggest_categorical('steps', (10, 25, 50, 100, 200))
     )
-
-    # Optimize the CNN hyperparameters
-    learning_rate = trial.suggest_float('learning_rate', 1e-5, 1e-2, log=True)
-    batch_size = trial.suggest_categorical('batch_size', [1, 2, 4, 8, 16, 32, 64, 128])
-    steps = trial.suggest_categorical('steps', [10, 25, 50, 100, 200])
-
-    l1 = trial.suggest_float('l1', 1e-10, 1e-2, log=True)
-    l2 = trial.suggest_float('l2', 1e-10, 1e-2, log=True)
     
-    filter_nums = [
-        trial.suggest_categorical('filter_num_1', [8, 16, 32]),
-        trial.suggest_categorical('filter_num_2', [16, 32, 64]),
-        trial.suggest_categorical('filter_num_3', [32, 64, 128]),
-        trial.suggest_categorical('filter_num_3', [64, 128, 256]),
-    ]
-    
-    filter_size = trial.suggest_categorical('filter_size', [(2, 2), (3, 3), (4, 4), (5, 5)])
-
-
-    # Build the CNN model with the suggested hyperparameters
-    model = cnn(
-        frames=config.FRAMES,
-        wavelengths=config.WAVELENGTHS,
-        learning_rate=learning_rate,
-        l1=l1,
-        l2=l2,
-        filter_nums=filter_nums,
-        filter_size=filter_size
-    )
-
-    # Set early stopping callback
-    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
-        monitor='val_loss',
-        patience=10,
-        min_delta=0.000005,
-        mode='min',
-        verbose=1,
-        restore_best_weights=True
-    )
-
-    # Set tensorboard callback
-    log_dir = config.TENSORBOARD_LOG_DIR + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(
-        log_dir=log_dir,
-        histogram_freq=1
-    )
-
-    # Train the model
-    model.fit(
-        training_dataset.batch(batch_size),
-        validation_data=validation_dataset.batch(batch_size),
-        epochs=config.EPOCHS,
-        steps_per_epoch=steps,
-        validation_steps=steps,
-        verbose=0,
-        callbacks=[early_stopping_callback, tensorboard_callback]
-    )
-
-    # Evaluate the model on the validation dataset and return the RMSE
-    rmse = model.evaluate(
-        validation_dataset.batch(batch_size),
-        return_dict=True,
-        verbose=0
-    )['RMSE']
-
-    print(f'Trial {trial.number}: Validation RMSE={rmse:.5f} with params: '
-          f'learning_rate={learning_rate}, l1={l1}, l2={l2}, '
-          f'filter_nums={filter_nums}, filter_size={filter_size}')
-
     return rmse
