c18a52babcafff216f8cb448737082b7ae3b05d7
George Perdrizet <george@perdrizet.org>
2025-09-19 22:49:30 -0400

    Decreased number of parallel workers to 1 per GPU for large network experiments

--- ariel.py
+++ ariel.py
@@ -50,8 +50,8 @@ if __name__ == '__main__':
         clear_tensorboard_logs()
         start_time = time.time()
 
-        with mp.Pool(processes=6) as pool:
-            pool.map(optimize_cnn.run, range(6))
+        with mp.Pool(processes=2) as pool:
+            pool.map(optimize_cnn.run, range(2))
 
         elapsed_time = time.time() - start_time
         print(f'\nCNN hyperparameter optimization complete in {elapsed_time/(60 * 60):.2f} hours\n')
